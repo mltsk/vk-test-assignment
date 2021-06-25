cf43a577c532fbd0cb3bd0015398f79589b032cd
Dmitrii Maletskii <mltsk94@gmail.com>
2021-06-25 22:48:32 +0300

    save before test

--- module/formGenerator.js
+++ module/formGenerator.js
@@ -3,10 +3,12 @@ import htmlGenerator from "./htmlGenerator.js";
 
 const formGenerator = (json, style) => {
     const html = htmlGenerator(json);
-    const css = `<style>\n${styleGenerator(json, style)}\n</style>`;
-    html.insertAdjacentHTML('beforeend', css);
+    // const css = `<style>\n${styleGenerator(json, style)}\n</style>`;
+    // html.insertAdjacentHTML('beforeend', css);
     const htmlAndCss = html.outerHTML.replace(/></gm, '>\n<');
+    // return htmlAndCss;
     return htmlAndCss;
+    console.log('html: ', html);
 }
 
 export default formGenerator;
--- module/htmlGenerator.js
+++ module/htmlGenerator.js
@@ -12,51 +12,57 @@ const createElementWithAttributes = (json) => {
 }
 
 const htmlGenerator = (json) => {
-    const result = document.createElement('form');
-    const iter = (json, result) => {
-        const element = createElementWithAttributes(json);
-        if(json.text !== undefined) element.textContent = json.text;
-        if(json.element === 'select') {
-            json.option.forEach(item => {
-                if (typeof(item) === 'string') {
-                    const option = document.createElement('option');
-                    option.textContent = item
-                    element.appendChild(option);
-                } else {
-                    const obj = Object.assign(item, {element: 'option'});
-                    const option = createElementWithAttributes(obj);
-                    option.textContent = obj.text;
-                    element.appendChild(option);
-                }
-            })
-        }
-
-        if(json.label !== undefined) {
-            let label = document.createElement('label');
-            label.setAttribute('for', json.id);
-            label.innerText = json.label;
-            result = result.appendChild(document.createElement('p'));
-            result.appendChild(label);
-            result.appendChild(element);
-        } else if (json.legend !== undefined) {
-            let legend = document.createElement('legend');
-            legend.innerText = json.legend;
-            result = result.appendChild(element);
-            result.appendChild(legend);
-            
-        } else  {
-            result = result.appendChild(element);
-        }
-
-        if(json.elements !== undefined) {
-                json.elements.forEach(item => {
-                    result.appendChild(iter(item, element));
+    let result = document.createElement('form');
+    console.log('result0: ', result);
+    const iter = (jsonArr, result) => {
+        // console.log('result2: ', result);
+        // let node = '';
+        jsonArr.forEach(json => {
+            const element = createElementWithAttributes(json);
+            if(json.text !== undefined) element.textContent = json.text;
+            if(json.element === 'select') {
+                json.option.forEach(item => {
+                    if (typeof(item) === 'string') {
+                        const option = document.createElement('option');
+                        option.textContent = item
+                        element.appendChild(option);
+                    } else {
+                        const obj = Object.assign(item, {element: 'option'});
+                        const option = createElementWithAttributes(obj);
+                        option.textContent = obj.text;
+                        element.appendChild(option);
+                    }
                 })
-        }
-        return result;
-    }
+            }
 
-    return iter(json, result);
+            if(json.label !== undefined) {
+                let label = document.createElement('label');
+                label.setAttribute('for', json.id);
+                label.innerText = json.label;
+                const p = (document.createElement('p'));
+                p.appendChild(label);
+                p.appendChild(element);
+                result.appendChild(p);
+
+            } else if (json.legend !== undefined) {
+                let legend = document.createElement('legend');
+                legend.innerText = json.legend;
+                element.appendChild(legend);
+                result.appendChild(element);
+                
+            } else  {
+                result.appendChild(element);
+            }
+
+            if(json.elements !== undefined) {
+                // console.log('json.elements: ', json.elements);
+                (iter(json.elements, result));
+            }
+            // console.log('result11: ', result);
+        })
+        return result; 
+    }
+    return iter(json.elements, result);
 }
 
 export default htmlGenerator;
